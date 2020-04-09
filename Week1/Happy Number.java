7f45136894863cf235bbbe784aec7f6e22f6cb84
Youssef Kishk <ykishk@gmail.com>
2020-04-09 12:56:56 +0200

    Create Happy Number.java

---
+++ Week1/Happy Number.java
@@ -0,0 +1,14 @@
+ public boolean isHappy(int n) {
+	if (n == 1)
+		return true;
+
+	if(n == 4 ) 
+		return false;
+
+	int sum = 0;
+	while (n > 0) {
+		sum = sum + (n % 10) * (n % 10);
+		n /= 10;
+	}
+	return isHappy(sum);
+    }
